ca3fbc94e79420fe1e8b2b2d447eef4181108ee4
Christopher <christophertrevilla@gmail.com>
2024-05-05 15:59:09 -0600

    :sparkles: feat: added database util

---
+++ src/utils/Database.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import contexts.Context;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Database {
+
+    private Context context;
+
+    public Context context() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
