35da89ddc29e0d41d823e5284c0222401e827217
Christopher <christophertrevilla@gmail.com>
2024-05-05 17:35:31 -0600

    :sparkles: feat: added router

---
+++ src/utils/Page.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author Christopher
+ */
+public interface Page {
+
+    public String pageName();
+
+    public void setRouter(Router router);
+
+    public void setVisible(boolean flag);
+}
---
+++ src/utils/Router.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.util.List;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Router {
+
+    private List<Page> pages;
+
+    public void addPage(Page page) {
+        this.pages.add(page);
+    }
+
+    public void setPages(List<Page> pages) {
+        this.pages = pages;
+    }
+
+    public void redirect(String pageName) {
+        for (var page : this.pages) {
+            if (pageName.equals(page.pageName())) {
+                page.setVisible(true);
+            } else {
+                page.setVisible(false);
+            }
+        }
+    }
+
+    public void moveToProductoView() {
+        this.redirect("ProductoView");
+    }
+
+    public void moveToPedidoView() {
+        this.redirect("PedidoView");
+    }
+
+    public void moveToClienteView() {
+        this.redirect("ClienteView");
+    }
+
+    public void moveToLoginView() {
+        this.redirect("LoginView");
+    }
+
+    public void moveToHomeView() {
+        this.redirect("HomeView");
+    }
+
+}
