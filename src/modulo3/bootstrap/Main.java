362887c1d2167d76b6c22feb6f51eb1c209b2498
Christopher <christophertrevilla@gmail.com>
2024-05-05 17:36:17 -0600

    :sparkles: feat: added first assembly

--- src/modulo3/bootstrap/Main.java
+++ src/modulo3/bootstrap/Main.java
@@ -4,17 +4,50 @@
  */
 package modulo3.bootstrap;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+import utils.Database;
+import utils.Router;
+import views.ClienteView;
+import views.HomeView;
+import views.LoginView;
+import views.PedidoView;
+import views.ProductoView;
+
 /**
  *
  * @author Christopher
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        var propsInput = new FileInputStream("config.properties");
+        var configuration = new Properties();
+        configuration.load(propsInput);
+
+        var router = new Router();
+
+        var loginView = new LoginView();
+        loginView.setRouter(router);
+
+        var homeView = new HomeView();
+        homeView.setRouter(router);
+
+        var clienteView = new ClienteView();
+        clienteView.setRouter(router);
+
+        var pedidoView = new PedidoView();
+        pedidoView.setRouter(router);
+
+        var productoView = new ProductoView();
+        productoView.setRouter(router);
+
+        router.setPages(new ArrayList(Arrays.asList(loginView, homeView, clienteView, pedidoView, productoView)));
+        router.moveToLoginView();
     }
-    
+
 }
