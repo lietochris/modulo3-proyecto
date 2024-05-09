9c21b64053e39090ef376b278840599b9072cd9f
Mayte Chavez Salazar <Mayte Chavez Salazar@DESKTOP-4QDNPCB>
2024-05-09 15:42:32 -0600

    Add methods CRUD for supplier

--- src/presenter/ProveedorPresenter.java
+++ src/presenter/ProveedorPresenter.java
@@ -4,11 +4,15 @@
  */
 package presenter;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.Proveedor;
 import repositories.ProveedorRepository;
+import utils.Result;
 
 /**
  *
- * @author Christopher
+ * @author Christopher / Mayte Chavez Salazar
  */
 public class ProveedorPresenter {
 
@@ -16,6 +20,62 @@ public class ProveedorPresenter {
 
     public ProveedorPresenter(repositories.ProveedorRepository repository) {
         this.repository = repository;
+    }
+    
+            /*Obtiene todos los registros*/
+    public List<Proveedor> FindAll() {
+        var result = this.repository.findAll();
+        return !result.isError() ? new ArrayList<>() : result.value();
+    }
+    
+    /*
+    Obtiene datos del proveedor por Id
+    */
+    
+    public Result<Proveedor> FindSupplierById(int id){
+        var result = this.repository.findById(id);
+        return result.isError() ? new Result(result.error()) : new Result(result.value());
+    }
+    
+    /* Crea un nuevo proovedor */
+    public Result<String> CreateSupplier(Proveedor nuevoProveedor){
+        var result = this.repository.findById(nuevoProveedor.idProveedor());
+        
+        if (result.isError() && result.error().code().equals("NOT_FOUND")) {
+            return new Result(utils.Error.make("SUPPLIER_EXISTS", "El proveedor ya existe"));
+        }
+
+        this.repository.create(nuevoProveedor); 
+
+        return new Result("El proveedor fue creado");
+    }
+    
+    /*Elimina un proveedor por id*/
+    public Result<String> DeleteSupplier(int id){
+        var result = this.repository.findById(id);
+        
+        if (result.isError() && result.error().code().equals("NOT_FOUND")) {
+            return new Result(utils.Error.make("SUPPLIER_NOT_EXISTS", "El proveedor no existe"));
+        }
+        return new Result("Proveedor elimiando correctamente");
+    }
+    
+    /* Actualizar proveedor*/
+    public Result<Proveedor> UpdateSupplier(int id, Proveedor proveedor){
+        var result = this.repository.findById(id);
+        
+        if(result.isError() && result.error().code().equals("NOT_FOUND")){
+            return new Result(utils.Error.make("SUPPLIER_NOT_EXISTS", "El proveedor no existe"));
+        }
+        var proveedorActualizado = this.repository.update(id, proveedor);
+        return new Result(proveedorActualizado);
+    }
+    
+    /*
+    Crea un reporte
+     */
+    public void CreateReport() {
+
     }
 
 }
